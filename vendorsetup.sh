c4836e5e40cfe00da183b01e6869db37e331b3dd
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2024-10-31 09:32:20 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,3 @@
-
 # 17.0.4 Clang
 rm -rf prebuilts/clang/host/linux-x86/r498229b
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 14.0 prebuilts/clang/host/linux-x86/r498229b
